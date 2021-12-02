e37b1ba1c448ce8f76b7ec8c434fd69a237136cc
Kevin Doan <kevinmndoan@gmail.com>
2021-12-01 23:44:45 -0800

    Logout functionality

--- app/src/main/java/com/example/food4cheap/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/food4cheap/fragments/ProfileFragment.java
@@ -1,14 +1,21 @@
 package com.example.food4cheap.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.food4cheap.LoginActivity;
 import com.example.food4cheap.R;
+import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +23,9 @@ import com.example.food4cheap.R;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+
+    private Button btnLogout;
+    private TextView tvUser;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -26,5 +36,29 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnLogout = view.findViewById(R.id.btnLogout);
+        tvUser = view.findViewById(R.id.tvUser);
+
+        tvUser.setText(ParseUser.getCurrentUser().getUsername());
+
+        // When logged out, send User back to Login Screen
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                goLoginActivity();
+            }
+        });
+    }
+    private void goLoginActivity() {
+        Intent i = new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+        getActivity().finish();
     }
 }
